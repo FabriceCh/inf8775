bbf08c4173a586ad3d818f63fb575fe2ebf3d732
Joel <joel.poulin@polymt.ca>
2019-01-28 15:10:48 -0500

    Tri rapide C++ (en cours).

--- tp1/tp1-H19/main.cpp
+++ tp1/tp1-H19/main.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <utility>
 
 using namespace std;
 
-int* chargerDonnees(){
+int triRapideRecursif(int tableau_lu[], int pointeur_gauche, int pointeur_droit){
+    int pivot = tableau_lu[pointeur_droit];
+    int i = (pointeur_gauche - 1);
+    for (int j = pointeur_gauche; j <= pointeur_droit - 1; j++){
+        if (tableau_lu[j] <= pivot){
+            i++;
+            swap(tableau_lu[i], tableau_lu[j]);
+        }
+    }
+    swap(tableau_lu[i + 1], tableau_lu[pointeur_droit]);
+    return (i + 1);
+}
+
+void triRapide(int tableau_lu[], int pointeur_gauche, int pointeur_droit){
+    int pivot = triRapideRecursif(tableau_lu, pointeur_gauche, pointeur_droit);
+    if (pointeur_gauche < pointeur_droit){
+        triRapide(tableau_lu, pointeur_gauche, pivot - 1);
+        triRapide(tableau_lu, pivot + 1, pointeur_droit);
+    }
+}
+
+int main() {
+    int tableau_lu[1000];
     ifstream fichier;
-    unsigned int value;
-    int* tableau_lu = new int[1000];
+    unsigned int nombre;
     fichier.open("../exemplaires//testset_1000_0.txt");
     if(!fichier){
         cerr << "Error opening file";
     }
     unsigned int compteur = 0;
-    while (fichier >> value){
-        tableau_lu[compteur] = value;
+    while (fichier >> nombre){
+        tableau_lu[compteur] = nombre;
         compteur++;
     }
     fichier.close();
-    return tableau_lu;
-}
-int main() {
-    int* tableau_lu = chargerDonnees();
-    for (int i = 0; i < 1000; i++){
+    int pointeur_gauche = 1;
+    int pointeur_droit = 999;
+    triRapide(tableau_lu, pointeur_gauche, pointeur_droit);
+    for (int i = 0; i < 1000; i++) {
         cout << tableau_lu[i] << ", ";
     }
-    return 0;
 }
